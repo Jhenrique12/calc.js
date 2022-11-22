27042c636105be2808dadf66c40e4faca2bcc569
Jhenrique12 <jonhenrique007@gmail.com>
2022-11-22 11:40:52 -0300

    Add: functio to switch theme and to copy result to clipboard

--- script.js
+++ script.js
@@ -38,3 +38,31 @@ function calculate () {
   const result = eval(input.value)
   resultInput.value = result
 }
+
+document.getElementById("copyToClipboard").addEventListener("click", function (ev) {
+  const button = ev.currentTarget
+  if (button.innerText === "Copy") {
+    button.innerText = "Copied!"
+    button.classList.add("success")
+    navigator.clipboard.writeText(resultInput.value)
+  } else {
+    button.innerText = "Copy"
+    button.classList.remove("success")
+  }
+})
+
+document.getElementById("themeSwitcher").addEventListener("click", function () {
+  if (main.dataset.theme === "dark") {
+    root.style.setProperty("--bg-color", "#f1f5f9")
+    root.style.setProperty("--border-color", "#aaa")
+    root.style.setProperty("--font-color", "#212529")
+    root.style.setProperty("--primary-color", "#0a7061")
+    main.dataset.theme = "light"
+  } else {
+    root.style.setProperty("--bg-color", "#212529")
+    root.style.setProperty("--border-color", "#666")
+    root.style.setProperty("--font-color", "#f1f5f9")
+    root.style.setProperty("--primary-color", "#0ce7c6")
+    main.dataset.theme = "dark"
+  }
+})
